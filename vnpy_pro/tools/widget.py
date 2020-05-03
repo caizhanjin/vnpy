75f3d3d6cd5a11c58479cd62903c2f40139f046b
caizhanjin <1362696697@qq.com>
2020-05-03 11:17:54 +0800

    [mod]优化get_logger

--- vnpy_pro/tools/widget.py
+++ vnpy_pro/tools/widget.py
@@ -1,10 +1,14 @@
 import logging
 from datetime import datetime
 from logging import INFO, DEBUG
+from typing import Dict
 
 from vnpy.trader.event import EVENT_LOG
 from vnpy.trader.setting import SETTINGS
 from vnpy.trader.utility import get_folder_path
+
+
+file_handlers: Dict[str, logging.FileHandler] = {}
 
 
 def get_logger(logger_name="default_logger", file_path=None,
@@ -26,11 +30,13 @@ def get_logger(logger_name="default_logger", file_path=None,
             log_path = get_folder_path("log")
             file_path = log_path.joinpath(filename)
 
-        file_handler = logging.FileHandler(
-            file_path, mode="a", encoding="utf8"
-        )
-        file_handler.setLevel(level)
-        file_handler.setFormatter(formatter)
+        file_handler = file_handlers.get(file_path, None)
+        if file_handler is None:
+            file_handler = logging.FileHandler(
+                file_path, mode="a", encoding="utf8"
+            )
+            file_handler.setLevel(level)
+            file_handler.setFormatter(formatter)
         current_logger.addHandler(file_handler)
 
     return current_logger
@@ -38,5 +44,6 @@ def get_logger(logger_name="default_logger", file_path=None,
 
 if __name__ == '__main__':
     default_logger = get_logger()
+    default_logger2 = get_logger()
     default_logger.info("hello world.")
     pass
